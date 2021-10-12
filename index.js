3e31481ec23a1120b55b5d1e4279010e60d0e2ba
nicoledodge <nicoledodge5@gmail.com>
2021-10-11 20:22:08 -0500

    initial pseudo-coding

---
+++ index.js
@@ -0,0 +1,23 @@
+// GIVEN a command-line application that accepts user input
+// WHEN I am prompted for information about my application repository
+// THEN a high-quality, professional README.md is generated with the title of my project and sections entitled Description, Table of Contents, Installation, Usage, License, Contributing, Tests, and Questions
+// WHEN I enter my project title
+// THEN this is displayed as the title of the README
+// WHEN I enter a description, installation instructions, usage information, contribution guidelines, and test instructions
+// THEN this information is added to the sections of the README entitled Description, Installation, Usage, Contributing, and Tests
+// WHEN I choose a license for my application from a list of options
+// THEN a badge for that license is added near the top of the README and a notice is added to the section of the README entitled License that explains which license the application is covered under
+// WHEN I enter my GitHub username
+// THEN this is added to the section of the README entitled Questions, with a link to my GitHub profile
+// WHEN I enter my email address
+// THEN this is added to the section of the README entitled Questions, with instructions on how to reach me with additional questions
+// WHEN I click on the links in the Table of Contents
+// THEN I am taken to the corresponding section of the README
+
+// write individual functions for different questions and label them a variable of "questions"
+// if left blank, throw error message for user to put at least a place holder? or create placeholder if user leaves blank?
+//function to call every function in a row - possibly with for each loop
+//append markdown file to something and add gitignore files
+//functions to edit the format of markdown - include social & deployed page links, place holder for gif demo, table of contents
+//Create a .gitignore file and include node_modules/ and .DS_Store/ so that your node_modules directory isn't tracked or uploaded to GitHub. Be sure to create your .gitignore file before installing any npm dependencies.
+//Make sure that your repo includes a package.json
